1dd048185bdbb1fc25c0d02e1e858241988b1970
Vladislav Kuznetsov <headgenrekty+github@gmail.com>
2025-03-02 13:55:42 +0400

    Added login/register forms

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'myapp',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -125,3 +128,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
