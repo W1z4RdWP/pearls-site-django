7a50fb7c1d89e240dc46f990453fb2e058072798
Vladislav <vladislavOps0@yandex.ru>
2025-06-29 16:16:29 +0000

    хэндлер 403 ошибки

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -39,6 +39,8 @@ def is_author_or_admin(user, course):
 def page_not_found_view(request, exception):
     return render(request, '404.html', status=404)
 
+def permission_denied_view(request, exception=None):
+    return render(request, '403.html', status=403)
 
 class ChangelogListView(ListView):
     model = ChangeLog
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -52,6 +52,7 @@ urlpatterns = [
 ]
 
 handler404 = 'myapp.views.page_not_found_view'
+handler403 = 'myapp.views.permission_denied_view'
 
 if settings.DEBUG:
     urlpatterns.extend(debug_toolbar_urls())
