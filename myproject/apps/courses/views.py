b5bbd3aac4aafc00fb5aa1a9ec042a6a3bd7e5d4
Vladislav <vladislavOps0@yandex.ru>
2025-09-16 11:31:20 +0000

    Формулы для закрашенных ячеек готовы

--- myproject/apps/courses/views.py
+++ myproject/apps/courses/views.py
@@ -1751,6 +1751,24 @@ def export_metrics_to_excel(request, submission_id):
             ws[f'E{row_num}'].number_format = '0%'
             ws[f'F{row_num}'].number_format = '0%'
         
+        # Добавляем суммарные значения для блока 4 (Загрузка доктора пациентами) в строку заголовка
+        load_header_row = current_row - max_doctors - 1  # строка заголовка таблицы
+        for month_idx in range(3):
+            # Формула: закрашенная ячейка из таблицы "Кол-во часов с пациентами" / закрашенная ячейка из таблицы "Кол-во часов по графику"
+            patient_sum_cell = f'{chr(68+month_idx)}{patient_header_row}'  # суммарная ячейка из таблицы "Кол-во часов с пациентами"
+            schedule_sum_cell = f'{chr(68+month_idx)}7'  # суммарная ячейка из таблицы "Кол-во часов по графику" (строка 7)
+            formula = f"=IF({schedule_sum_cell}=0,0,{patient_sum_cell}/{schedule_sum_cell})"
+            ws[f'{chr(68+month_idx)}{load_header_row}'] = formula
+            # Применяем процентный формат к суммарным ячейкам
+            ws[f'{chr(68+month_idx)}{load_header_row}'].number_format = '0%'
+        
+        # Добавляем формулу среднего значения в столбец H для таблицы "Загрузка доктора пациентами"
+        # Формула: среднее значение из таблицы "Кол-во часов с пациентами" / среднее значение из таблицы "Кол-во часов по графику" (H7)
+        patient_avg_cell = f'H{patient_header_row}'  # среднее из таблицы "Кол-во часов с пациентами"
+        schedule_avg_cell = 'H7'  # среднее из таблицы "Кол-во часов по графику"
+        ws[f'H{load_header_row}'] = f"=IF({schedule_avg_cell}=0,0,{patient_avg_cell}/{schedule_avg_cell})"
+        ws[f'H{load_header_row}'].number_format = '0%'
+        
         # Пропускаем 2 строки
         current_row += 2
         
@@ -1788,6 +1806,21 @@ def export_metrics_to_excel(request, submission_id):
             ws[f'D{row_num}'].number_format = '0.00'
             ws[f'E{row_num}'].number_format = '0.00'
             ws[f'F{row_num}'].number_format = '0.00'
+        
+        # Добавляем суммарные значения для блока 5 (Средний час) в строку заголовка
+        avg_hour_header_row = current_row - max_doctors - 1  # строка заголовка таблицы
+        for month_idx in range(3):
+            # Формула: закрашенная ячейка из таблицы "Выручка" / закрашенная ячейка из таблицы "Кол-во часов с пациентами"
+            revenue_sum_cell = f'{chr(68+month_idx)}{revenue_header_row}'  # суммарная ячейка из таблицы "Выручка"
+            patient_sum_cell = f'{chr(68+month_idx)}{patient_header_row}'  # суммарная ячейка из таблицы "Кол-во часов с пациентами"
+            formula = f"=IF({patient_sum_cell}=0,0,{revenue_sum_cell}/{patient_sum_cell})"
+            ws[f'{chr(68+month_idx)}{avg_hour_header_row}'] = formula
+            # Применяем формат с 2 знаками после запятой к суммарным ячейкам
+            ws[f'{chr(68+month_idx)}{avg_hour_header_row}'].number_format = '0.00'
+        
+        # Добавляем формулу среднего значения в столбец H для таблицы "Средний час"
+        ws[f'H{avg_hour_header_row}'] = "=AVERAGE(D{0}:F{0})".format(avg_hour_header_row)
+        ws[f'H{avg_hour_header_row}'].number_format = '0.00'
         
         # Обновляем формулы загрузки клиники по месяцам (только первые 3 месяца)
         for month_idx in range(3):
