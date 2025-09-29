7cc85b99b5a1ddbd679074b2fa2ae5b0b2fa9ab8
Administrator <gitlab_admin_519208@example.com>
2025-09-29 14:44:59 +0000

    Edit update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 
 set -e  # Прервать выполнение при ошибке
 
-git pull
+git pull origin main
 
 source .venv/bin/activate # Активация виртуального окружения
 cd myproject # Переход в директорию проекта
