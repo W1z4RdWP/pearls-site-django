8ecfe9fee743503455af933bfa9970b52608d9b0
Vladislav <vladislavOps0@yandex.ru>
2025-10-09 09:35:56 +0000

    Исправлен редирект на главную дизайн страницу, для внешних пользователей

--- myproject/apps/users/views.py
+++ myproject/apps/users/views.py
@@ -218,8 +218,8 @@ class CustomLoginView(LoginView):
                 }
             )
             auth_login(self.request, user)
-            # Редирект на курс чек-апа для внешних пользователей
-            return redirect('courses:course_detail', slug='chek-ap-stomatologicheskoi-kliniki')
+            # Редирект после авторизации для внешних пользователей
+            return redirect('homepage')
         
         audit_logger.info(
             'Вошёл в систему', 
