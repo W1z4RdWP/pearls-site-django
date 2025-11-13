8934bfcbd0c938ba2bc7c77a46cdeb868364f9a6
Vladislav <vladislavOps0@yandex.ru>
2025-11-13 15:27:54 +0400

    Добавенлы фото профиля, в уведомление о проверке открытого теста пользователя

--- myproject/apps/notifications/views.py
+++ myproject/apps/notifications/views.py
@@ -105,7 +105,7 @@ def notification_count(request):
 def notification_dropdown(request):
     """Частичное представление для выпадающего списка уведомлений"""
     # Показываем последние 5 уведомлений (все, не только непрочитанные)
-    notifications = Notification.objects.filter(user=request.user).order_by('-created_at')[:5]
+    notifications = Notification.objects.filter(user=request.user).select_related('related_quiz_result__reviewed_by__profile').order_by('-created_at')[:5]
     total_unread = Notification.objects.filter(user=request.user, is_read=False).count()
     
     context = {
