a4faf4f8efc9ce7bb3f345eaccb1c4b2737e082b
Vladislav <vladislavOps0@yandex.ru>
2025-11-27 13:26:58 +0400

    Добавлены карточки товаров в магазин

--- myproject/apps/shop/admin.py
+++ myproject/apps/shop/admin.py
@@ -1,3 +1,56 @@
 from django.contrib import admin
+from .models import InternalProduct
 
-# Register your models here.
+
+@admin.register(InternalProduct)
+class InternalProductAdmin(admin.ModelAdmin):
+    """Административная панель для управления товарами магазина"""
+    
+    list_display = [
+        'id',
+        'name',
+        'points_price',
+        'constraints',
+        'is_active',
+        'created_at',
+    ]
+    
+    list_filter = [
+        'is_active',
+        'constraints',
+        'created_at',
+    ]
+    
+    search_fields = [
+        'name',
+        'description',
+        'restrictions_text',
+    ]
+    
+    readonly_fields = [
+        'created_at',
+        'updated_at',
+    ]
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'description', 'points_price', 'image', 'is_active')
+        }),
+        ('Ограничения', {
+            'fields': ('constraints', 'restrictions_text'),
+            'description': 'Укажите частоту использования и подробное описание ограничений'
+        }),
+        ('Системная информация', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    date_hierarchy = 'created_at'
+    ordering = ['-created_at']
+    list_per_page = 50
+    
+    def get_queryset(self, request):
+        """Оптимизация запросов"""
+        qs = super().get_queryset(request)
+        return qs
--- myproject/apps/shop/views.py
+++ myproject/apps/shop/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
-
+from .models import InternalProduct
 
 
 class ShopView(TemplateView):
     """Класс представление страницы магазина"""
     template_name = 'shop/shop.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Получаем только активные товары
+        context['products'] = InternalProduct.objects.filter(is_active=True)
+        return context
