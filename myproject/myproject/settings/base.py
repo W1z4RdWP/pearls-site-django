8fea772c6368eeed6c3649c3ea9485e6adbb4bd8
Vladislav <vladislavOps0@yandex.ru>
2026-02-26 13:17:26 +0400

    Добавлен параметр, для игнорирования падения redis

--- myproject/myproject/settings/base.py
+++ myproject/myproject/settings/base.py
@@ -208,6 +208,7 @@ CACHES = {
         'LOCATION': os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/1'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+            'IGNORE_EXCEPTIONS': True,
             # Если нужен пароль:
             # 'PASSWORD': os.getenv('REDIS_PASSWORD'),
         }
